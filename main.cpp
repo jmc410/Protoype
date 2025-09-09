05fa9363d4d008122bd08ca77a2876cf55a64988
jmc410 <jmc41024@email.vccs.edu>
2025-09-09 07:47:26 +0000

    Yes-2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-int main() { std::cout << "Hello World!.\n"; 
-            std::cout << "Yoooo.\n";
+int main() { std::cout << "Jaden Keane Cumaldi.\n"; 
+            std::cout << "757-949-6605.\n";
+            std::cout << "913 74th St.\n";
 
            }
